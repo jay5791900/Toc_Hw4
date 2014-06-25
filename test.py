3f0fa4c0655222cce6d4dd7eeedd4181f96a734d
JayFeng <jay579190@hotmail.com>
2014-06-26 00:40:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,39 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
+#-----------------------
+#Name:Guan-Jie Feng
+#Id:F74001161
+#Description:input url to get road information
+#-----------------------
 import json
 import urllib2
 import sys
 
 
-roadinfo = dict()
+roadinfo = dict()		#store road information
+roadprice = dict()		#soore road price
 
+
+def findresult(info):	#func to find max and min price , print the result
+	max_price = 0
+	min_price = 10000000000000000000000
+	for item in roadprice:
+		if item == info:
+			for p in roadprice[info]:
+				if p > max_price:
+					max_price = p
+				if p < min_price:
+					min_price = p
+	print u"%s, 最高成交價:%d, 最低成交價:%d" % (info,max_price,min_price)
 
 if __name__ == '__main__':
-	inpu = sys.argv
-	url = 'http://www.datagarage.io/api/5385b69de7259bb37d925971'
-	data = urllib2.urlopen(url) #get url
+	inpu = sys.argv				#input argv
+	url = inpu[1]				#get url
+	data = urllib2.urlopen(url) #get parse json file
 	j = json.load(data)			#load json file
 
 
 
-	for l in j:  # first loop to establish roadname list
+	for l in j:  				# for loop to establish roadinfo list
 		temp = l[u'土地區段位置或建物區門牌']
 		if ( temp.find(u'路')!=(-1) or temp.find(u'街')!=(-1) or temp.find(u'大道')!=(-1) or temp.find(u'巷')!=(-1)):
 			if ( temp.find(u'路')!=(-1) ):
@@ -28,36 +46,39 @@ if __name__ == '__main__':
 				key = u'巷'
 
 			if key == u'大道':			
-				name = l[u'土地區段位置或建物區門牌'][:temp.find(key)+2] #get
+				name = l[u'土地區段位置或建物區門牌'][:temp.find(key)+2] #get name
 				year = l[u'交易年月']
-			else: 
-				name = l[u'土地區段位置或建物區門牌'][:temp.find(key)+1] #get
-				year = l[u'交易年月']
+				price = l[u'總價元']
 
-			if roadinfo.has_key(name) == False:
+			else: 
+				name = l[u'土地區段位置或建物區門牌'][:temp.find(key)+1] 
+				year = l[u'交易年月']
+				price = l[u'總價元']
+
+			if roadinfo.has_key(name) == False:		#check if roadname exist
 				roadinfo[name] = []
 
-			if roadinfo[name].count(year) == 0:
+			if roadinfo[name].count(year) == 0:		#check if year  exist
 				roadinfo[name].append(year)
 
+			if roadprice.has_key(name) == False:	#check if roadname exist
+				roadprice[name] = []
+
+			if roadprice[name].count(price) == 0:	#check if price exist
+				roadprice[name].append(price)
+								
 		else: continue
 
 
-	print json.dumps(roadinfo,ensure_ascii=False,indent=2)
-	
-	max1 = 0
+	#print json.dumps(roadinfo,ensure_ascii=False,indent=2)
+
+	max_road = 0				#find the most trade of roadname
 	for i in roadinfo:
 		c = len(roadinfo[i])
-		if c > max1:
-			max1 = c
+		if c > max_road:
+			max_road = c
 
-
-	#t = u'臺北市北投區中央北路'
-	#x = u'臺北市中正區汀州路'
-
-	#for l in j:
-#		if x in l[u'土地區段位置或建物區門牌']:
-#			print l[u'土地區段位置或建物區門牌']
-		
-
-
+	#print max_road
+	for road in roadinfo:
+		if len(roadinfo[road]) == max_road:
+			findresult(road)	#find max and min price , print the result
